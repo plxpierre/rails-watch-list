c6615d8dc51f6960145b23c61e0b15be8e145c2e
Pierre Planex <pierre.planex@gmail.com>
2026-02-26 17:13:57 +0100

    watch l

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
