18e9526e4d3dbbad0981c79f1540746d5cd863dd
Pierre Planex <pierre.planex@gmail.com>
2026-02-27 16:08:24 +0100

    watch list up

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 // app/assets/config/manifest.js
+
 //= link_tree ../images
-//= link_tree ../../javascript .js
 //= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists do
+  root to: "lists#index"
+
+  resources :lists, except: [:index] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
